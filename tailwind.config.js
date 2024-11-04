fce1a7f61446b46709227871556498c8973d13f7
jesus Garcia <jesus.garcia2611@hotmail.com>
2024-11-04 05:23:21 -0500

    Add linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#91b07c',
-        primaryHover: '#48653c',
-        secondary: '#E5C6A4',
-        secondaryHover: '#9E6552'
-      },
-      backgroundColor: {
-        base: '#F4F0E3'
-      },
-      keyframes: {
-        'slide-in': {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0)' }
-        }
-      },
-      animation: {
-        'slide-in': 'slide-in 0.3s ease-out'
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#91b07c',
+      primaryHover: '#48653c',
+      secondary: '#E5C6A4',
+      secondaryHover: '#9E6552'
+    },
+    backgroundColor: {
+      base: '#F4F0E3'
+    },
+    keyframes: {
+      'slide-in': {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0)' }
       }
     },
+    animation: {
+      'slide-in': 'slide-in 0.3s ease-out'
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
