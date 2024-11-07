754c9b37338b91a74687d05527c52c6973665475
Josmar Diaz <diazjosmar80@gmail.com>
2024-11-07 17:29:32 -0500

    Mejora del responsive en producto de detalle

--- src/Components/ImageProduct.jsx
+++ src/Components/ImageProduct.jsx
@@ -1,16 +1,17 @@
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import { IoClose } from "react-icons/io5";
+import "../styles/productDetail.css"
 
 const ImageProduct = (props) => {
 
   return (
-      <div className='relative' style={{display: "flex", justifyContent: "center", flexDirection: "column", height: "100%"}}>
-        <div onClick={() => props.onClickClose()} className='text-xl cursor-pointer	text-white	font-bold	absolute top-[25px] right-3'><IoClose className="w-7 h-7" /></div>
+      <div className='relative flex justify-center flex-col h-full'>
+        <div onClick={() => props.onClickClose()} className='z-50 text-xl cursor-pointer	text-white	font-bold	absolute top-[25px] right-3'><IoClose className="w-7 h-7" /></div>
         <Slide autoplay={false} transitionDuration={1}>
             {props.imgs.map((val, key) => 
               <div key={key} className="flex justify-center	each-slide-effect">
-                <img style={{width: "90%"}} src={val} alt="" />
+                <img className='w-4/6' src={val} alt="" />
               </div>
 
             )}
