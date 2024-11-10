838bd45a8e3f05628852372bb9a7cd95cfe28c2a
Estefanía Aguas <estefaniaaguas@gmail.com>
2024-11-10 15:26:41 -0500

    feat: se crea componente userMenu

---
+++ src/Components/UserMenu.jsx
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import { routes } from "../utils/routes";
+import { useState, useRef, useEffect } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import {useAuthContext} from "../contexts/AuthContext.jsx";
+
+const UserMenu = ({ user }) => {
+
+    const {logout} = useAuthContext();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null); // Ref para el contenedor del menú
+    const navigate = useNavigate();
+    const initialLetters = user.name.charAt(0) + user.lastName.charAt(0).toUpperCase();
+
+    // Función para cerrar sesión
+    const handleLogout = () => {
+        // Lógica para cerrar sesión, puede ser un logout del contexto, o borrar el token
+        console.log("Usuario ha cerrado sesión");
+        logout();
+        navigate("/login");
+    };
+
+    // Función para abrir el menú
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    // Cierra el menú si el clic es fuera del mismo
+    useEffect(() => {
+        // Función que detecta clics fuera del menú
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        // Añadir el event listener
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Limpiar el event listener cuando el componente se desmonte
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className="relative flex items-center">
+            <div onClick={toggleMenu} className="flex items-center cursor-pointer gap-4">
+                <span className="font-bold bg-primaryHover rounded-full text-white w-10 h-10 flex items-center justify-center">
+                    {initialLetters}
+                </span>
+                {isMenuOpen ? (
+                    <IoIosArrowUp className="text-2xl text-primaryHover" />
+                ) : (
+                    <IoIosArrowDown className="text-2xl text-primaryHover" />
+                )}
+            </div>
+
+            {/* Menú desplegable */}
+            {isMenuOpen && (
+                <div ref={menuRef}
+                    className="absolute -right-6 top-20 bg-base rounded-lg shadow-lg w-fit">
+                    <ul className="py-6">
+                        <li className={"px-8 pt-2 pb-4 text-secondaryHover font-bold border-b border-secondary "}>
+                            Hola, {user.name}
+                        </li>
+                        <li className="px-8 py-2 text-nowrap cursor-pointer hover:bg-secondaryHover hover:text-white"
+                            onClick={() => navigate(routes.administrationHome)}>
+                            Panel de administración
+                        </li>
+                        <li className="px-8 py-2 text-nowrap cursor-pointer hover:bg-secondaryHover hover:text-white"
+                            onClick={handleLogout}>
+                            Cerrar sesión
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default UserMenu;
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -4,17 +4,25 @@ import {createContext, useContext, useEffect, useState} from 'react'
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true); // Para saber si los datos están cargando
-    
+
     const login = async (credentials) => {
         console.log("Login", credentials)
         // Aquí iría la lógica de autenticación real (ejemplo: llamada a la API)
-        const fakeUser = { id: 1, token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' };
+        const fakeUser = {
+            id: 1,
+            name: 'John',
+            lastName: 'Doe',
+            avatar: 'images/default_avatar.png',
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+        };
         setUser(fakeUser);
         localStorage.setItem('user', JSON.stringify(fakeUser)); // Guardamos el usuario en localStorage
+        setUser(fakeUser)
+
     };
 
     const logout = () => {
@@ -25,14 +33,14 @@ export const AuthProvider = ({ children }) => {
     useEffect(() => {
         const storedUser = localStorage.getItem('user');
         if (storedUser) {
-          setUser(JSON.parse(storedUser)); // Recuperamos el usuario del localStorage
+            setUser(JSON.parse(storedUser)); // Recuperamos el usuario del localStorage
         }
         setLoading(false); // Terminamos de cargar los datos
-      }, []);
-    
+    }, []);
+
 
     return (
-        <AuthContext.Provider value={{ user, login, logout, loading }}>
+        <AuthContext.Provider value={{user, login, logout, loading}}>
             {children}
         </AuthContext.Provider>
     )
