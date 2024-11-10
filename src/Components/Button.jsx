75d627de7eceff41c6f714d5e3c102a5b135467e
Estefanía Aguas <estefaniaaguas@gmail.com>
2024-11-10 12:56:13 -0500

    feat: se añade estilo a componente botón cuando está deshabilitado

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,12 +1,17 @@
-const Button = ({ type = 'primary', label, icon: Icon, onClick }) => {
+const Button = ({ type = 'primary', label, icon: Icon, onClick, disabled }) => {
   const baseStyles = 'flex items-center justify-center gap-2 px-4 rounded font-semibold transition duration-300 h-10 w-full';
   const primaryStyles = 'bg-primary text-white hover:bg-primaryHover';
   const secondaryStyles = 'bg-secondary text-white hover:bg-secondaryHover';
 
-  const buttonStyles = `${baseStyles} ${type === 'primary' ? primaryStyles : secondaryStyles}`;
+  const disabledStyles = 'bg-gray-300 text-gray-500 cursor-not-allowed';
+  const buttonStyles = `${baseStyles} ${type === 'primary' ? primaryStyles : secondaryStyles} ${disabled ? disabledStyles : ''}`;
+
 
   return (
-      <button className={buttonStyles} onClick={onClick}>
+      <button className={buttonStyles}
+              onClick={onClick}
+              disabled={disabled}
+      >
         {Icon && <Icon size={15} />}
         <span className="text-nowrap">{label}</span>
       </button>
